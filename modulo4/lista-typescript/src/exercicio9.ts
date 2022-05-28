65c28525a5d146c77569af56ad73e4be354b9330
Matheus Martinelle Barros <matheusmartinellibarros@gmail.com>
2022-05-28 19:05:25 -0300

    Lógica completa da 9

---
+++ modulo4/lista-typescript/src/exercicio9.ts
@@ -0,0 +1,55 @@
+// EXERCÍCIO 9 //
+
+/*
+    Uma operação matemática bastante utilizada em probabilidade e estatística é o **fatorial**, representado por um **!** (ponto de exclamação). Ele consiste em realizar a multiplicação de todos os números (a partir de 1) até aquele colocado na operação. Veja os exemplos abaixo:
+    
+    - `3! = 3*2*1 = 6`
+    - `4! = 4*3*2*1 = 24`
+    - `5! = 5*4*3*2*1 = 120`
+    - `6! = 6*5*4*3*2*1 = 720`
+    
+    Isso vale para todos os números inteiros não negativos (também chamados de "números naturais"). Dois valores para se tomar cuidado são:  `1! = 1` e `0! = 1` (uma exceção da regra!).
+    
+    Uma aplicação interessante do fatorial é o cálculo de anagramas de uma palavra. Anagrama é uma outra palavra (não precisa existir em português) com as mesmas letras da anterior em ordem diferentes. Por exemplo, anagramas da palavra `mesa` são: `ames`, `maes`, `meas`, `emsa`, `smea` e muitos outros.
+    
+    A quantidade de anagramas de uma palavra sem nenhuma letra repetida é o fatorial da quantidade de letras. Para `mesa`, a quantidade é `4! = 24`
+    
+    Considerando tudo o que foi mencionado, escreva uma função que receba uma `palavra` (sem letras repetidas) e devolva a quantidade de anagramas que ela possui.
+*/
+
+function anagram(palavra: string): number {
+    
+    const splits: string[] = palavra.toLocaleLowerCase().split('')
+    let result: number = splits.length
+    let count: number = 0
+
+    if(palavra.includes(' ')){
+        return undefined
+    } else if(palavra === ''){
+        return null
+    } else if(splits.length === 1){
+        return 1
+    } else {
+
+        while(count < splits.length){
+            let map: string[] = splits.filter((element) => {
+                return element === splits[count]
+            })
+            console.log(map)
+
+            if(map.length !== 1){
+                return undefined
+            } else {
+                count++
+            }
+        }
+
+        for(let i: number = 1; i < splits.length; i++){
+            result = result * i
+        }
+
+        return result        
+    }
+}
+
+console.table(anagram('Alsk'))
