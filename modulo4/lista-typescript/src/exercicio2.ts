1055c379545aaf47a6e510fe1598d5858c3b4dec
Matheus Martinelle Barros <matheusmartinellibarros@gmail.com>
2022-05-27 22:32:32 -0300

    Lista de Exercícios(typescript): 1-8

---
+++ modulo4/lista-typescript/src/exercicio2.ts
@@ -0,0 +1,9 @@
+// EXERCÍCIO 2 //
+    
+/*    Crie uma função que receba um parâmetro qualquer e que imprima no console o tipo da variável. */
+
+function qualquerCoisa(parametro: any) {
+    console.log(typeof parametro)
+}
+
+qualquerCoisa(1)
